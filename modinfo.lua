7ec688622d7ec81edffcc5db9f37a3f9f903d342
droooney <d.vor7@mail.ru>
2021-10-17 22:34:21 +0500

    add icon

--- modinfo.lua
+++ modinfo.lua
@@ -1,24 +1,13 @@
 name = "Buff Timer"
-
-description = "Cool mod by droooney"
-
-author = "droooney"
-
-version = "0.1"
-
+description = "Mod that shows information on the character's current buffs. The info is restored between shards and on connection"
+author = "jimmybaxter"
+version = "1.1"
 api_version = 10
-
 dst_compatible = true
-
 all_clients_require_mod = true
-
 client_only_mod = false
-
---icon_atlas = "modicon.xml"
---icon = "modicon.tex"
-
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
 server_filter_tags = {}
-
 forumthread = ""
-
 configuration_options = {}
